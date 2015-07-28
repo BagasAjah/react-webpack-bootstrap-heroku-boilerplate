5c0ba4233482cf3cb4c5f28dae44dfd9f25047f1
Xavier Carpentier <contact@xavier-carpentier.com>
2015-07-29 00:37:50 +0200

    feat(heroku): add buildpacks

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /(node_modules|bower_components)/,
-      loaders: ['react-hot', 'babel'],
+      loaders: ['babel'],
     }]
   },
   plugins: [
