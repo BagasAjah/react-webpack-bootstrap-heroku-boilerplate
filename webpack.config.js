11bed4383f3ffe2cc51d82133ddcfaa47377ff07
Xavier Carpentier <contact@xavier-carpentier.com>
2015-07-30 09:06:39 +0200

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,37 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: './index.jsx',
-  devtool: 'source-map',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.min.js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-    {test: /\.jsx?$/,exclude: /(node_modules|bower_components)/,loaders: ['babel']},
-    {test: /\.less$/,loader: "style!css!less"}
+    entry: './index.jsx',
+    devtool: 'source-map',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.min.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.less']
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loaders: ['babel']
+        }, {
+            test: /\.less$/,
+            loader: "style!css!less"
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }, {
+            test: /\.woff(2)?$/,
+            loader: "url-loader"
+        }, {
+            test: /\.(ttf|eot|svg)?$/,
+            loader: "file-loader"
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true
+        })
     ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({minimize: true})
-  ]
 };
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,36 +2,46 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
-    'webpack/hot/only-dev-server',
-    './index.jsx' // Your appʼs entry point
-  ],
-  devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
-  output: {
-    path: path.join(__dirname, 'client'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-    {test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loaders: ['react-hot', 'babel']},
-    {test: /\.less$/,loader: 'style!css!less'},
-    {test: /\.css$/,loader: 'style!css'},
-    { test: /\.woff(2)?$/, loader: "url-loader" },
-    { test: /\.(ttf|eot|svg)?$/, loader: "file-loader" }
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server',
+        './index.jsx' // Your appʼs entry point
+    ],
+    devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+    output: {
+        path: path.join(__dirname, 'client'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.less']
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loaders: ['react-hot', 'babel']
+        }, {
+            test: /\.less$/,
+            loader: 'style!css!less'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }, {
+            test: /\.woff(2)?$/,
+            loader: "url-loader"
+        }, {
+            test: /\.(ttf|eot|svg)?$/,
+            loader: "file-loader"
+        }]
+    },
+    devServer: {
+        contentBase: "./client",
+        noInfo: true, //  --no-info option
+        hot: true,
+        inline: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
     ]
-  },
-  devServer: {
-      contentBase: "./client",
-      noInfo: true, //  --no-info option
-      hot: true,
-      inline: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
 };
